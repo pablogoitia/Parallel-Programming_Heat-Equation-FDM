0e6129a4cd19a5c5d7dabdc872f375ca42151386
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-26 12:30:59 +0100

    Solved minor error

--- scripts/load_balancing/fdm_omp.sh
+++ scripts/load_balancing/fdm_omp.sh
@@ -7,7 +7,7 @@
 #SBATCH -o fdm_lb.out       # Name of stdout output file
 #SBATCH -e fdm_lb.err       # Name of stderr output file
 #SBATCH -N 8                # Total # of nodes
-#SBATCH --cpus-per-task 1   # Cores per task requested
+#SBATCH --cpus-per-task 4   # Cores per task requested
 #SBATCH -n 8                # Total # of mpi tasks
 #SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
 #SBATCH --nodelist=n16-80,n16-81,n16-82,n16-83,n16-90,n16-91,n16-92,n16-93

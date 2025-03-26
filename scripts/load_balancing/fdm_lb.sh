a96a1832254f3edf0482bca1d022dd8cc5872f42
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-26 12:19:11 +0100

    Upload new sbatch scripts for the load balancing analysis

---
+++ scripts/load_balancing/fdm_lb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#----------------------------------------------------
+# FDM MPI job script
+#----------------------------------------------------
+#SBATCH -J fdm_lb           # Job name
+#SBATCH -o fdm_lb.out       # Name of stdout output file
+#SBATCH -e fdm_lb.err       # Name of stderr output file
+#SBATCH -N 8                # Total # of nodes
+#SBATCH --cpus-per-task 1   # Cores per task requested
+#SBATCH -n 8                # Total # of mpi tasks
+#SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
+#SBATCH --nodelist=n16-80,n16-81,n16-82,n16-83,n16-90,n16-91,n16-92,n16-93
+#SBATCH --exclusive
+
+export OMP_NUM_THREADS=4
+
+prun ./fdm 0.001953125      # Run the program with 1/512
+echo "done"                 # Write this message on the output file when finished

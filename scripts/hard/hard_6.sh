0eb09cc6df08ec7ada928dc37a9a24a6a35950f6
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-26 09:52:02 +0100

    Added sbatch scripts

---
+++ scripts/hard/hard_6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#----------------------------------------------------
+# FDM MPI job script
+#----------------------------------------------------
+#SBATCH -J fdm_6          # Job name
+#SBATCH -o fdm_6.out      # Name of stdout output file(%j expands to jobId)
+#SBATCH -e fdm_6.err      # Name of stderr output file(%j expands to jobId)
+#SBATCH -N 1                # Total # of nodes
+#SBATCH --cpus-per-task 1   # Cores per task requested
+#SBATCH -n 6                # Total # of mpi tasks
+#SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
+#SBATCH -p lgpu             # Partition to submit to
+
+prun ./fdm_mpi 0.00390625   # Run the program with 1/256
+echo "done"                 # Write this message on the output file when finished

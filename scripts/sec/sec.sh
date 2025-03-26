e7f526018d24a0144e11a8d19b7ea8d605193bff
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-26 10:05:28 +0100

    Improved script

--- scripts/sec/sec.sh
+++ scripts/sec/sec.sh
@@ -13,11 +13,10 @@
 
 # Run for different values of n (powers of 2)
 # Probably it will not finish, but we will let it compute as much as it can
-for n in 4 8 16 32 64 128 256 512 1024 2048 4096
+for n in 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625
 do
-    value=$(echo "1/$n" | bc -l)
-    echo "Running with value: $value"
-    srun ./fdm_sec $value
+    echo "Running with value: $n"
+    srun ./fdm_sec $n
 done
 
 echo "done" # Write this message on the output file when finished

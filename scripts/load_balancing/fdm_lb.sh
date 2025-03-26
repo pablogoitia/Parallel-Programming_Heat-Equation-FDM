45bb36d6380b252334bbca657c1edbf52d3536a0
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-26 13:21:58 +0100

    Modified sbatch scripts for load balancing analysis

--- scripts/load_balancing/fdm_lb.sh
+++ scripts/load_balancing/fdm_lb.sh
@@ -3,17 +3,18 @@
 #----------------------------------------------------
 # FDM MPI job script
 #----------------------------------------------------
-#SBATCH -J fdm_lb           # Job name
-#SBATCH -o fdm_lb.out       # Name of stdout output file
-#SBATCH -e fdm_lb.err       # Name of stderr output file
-#SBATCH -N 8                # Total # of nodes
-#SBATCH --cpus-per-task 4   # Cores per task requested
-#SBATCH -n 8                # Total # of mpi tasks
+#SBATCH -J fdm_omp          # Job name
+#SBATCH -o fdm_omp.out      # Name of stdout output file
+#SBATCH -e fdm_omp.err      # Name of stderr output file
+#SBATCH -N 4                # Total # of nodes
+#SBATCH --cpus-per-task 4   # Cores per task (for OpenMP threads)
+#SBATCH -n 4                # Total # of MPI tasks
+#SBATCH --ntasks-per-node=1 # 1 MPI task per node
 #SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
-#SBATCH --nodelist=n16-80,n16-81,n16-82,n16-83,n16-90,n16-91,n16-92,n16-93
+#SBATCH --nodelist=n16-82,n16-83,n16-92,n16-93
 #SBATCH --exclusive
 
 export OMP_NUM_THREADS=4
 
-prun ./fdm 0.001953125      # Run the program with 1/512
-echo "done"                 # Write this message on the output file when finished
+prun ./fdm 0.0009765625      # Run the program with 1/1024
+echo "done"                  # Write this message on the output file when finished

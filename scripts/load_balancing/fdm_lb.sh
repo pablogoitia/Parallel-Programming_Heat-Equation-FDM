1a432ef196a4e49dd103f06b97590ee6d957c15a
Pablo Goitia <pgg566@alumnos.unican.es>
2025-03-27 23:41:24 +0100

    Upload results

--- scripts/load_balancing/fdm_lb.sh
+++ scripts/load_balancing/fdm_lb.sh
@@ -6,11 +6,12 @@
 #SBATCH -J fdm_lb           # Job name
 #SBATCH -o fdm_lb.out       # Name of stdout output file
 #SBATCH -e fdm_lb.err       # Name of stderr output file
-#SBATCH -N 8                # Total # of nodes
+#SBATCH -N 5                # Total # of nodes
 #SBATCH --cpus-per-task 4   # Cores per task (for OpenMP threads)
-#SBATCH -n 8                # Total # of MPI tasks
+#SBATCH -n 5                # Total # of MPI tasks
 #SBATCH --ntasks-per-node=1 # 1 MPI task per node
 #SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
+#SBATCH --nodelist=n16-80,n16-81,n16-90,n16-91,n16-92
 #SBATCH --exclusive
 
 export OMP_NUM_THREADS=4

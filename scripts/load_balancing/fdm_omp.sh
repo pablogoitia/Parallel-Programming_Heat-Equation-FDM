c874d80984fca44065649d4571323cb81c6fb0fa
Pablo Goitia <pablogoitia01@gmail.com>
2025-03-27 23:00:33 +0100

    Updated scripts for the load balancer testing

--- scripts/load_balancing/fdm_omp.sh
+++ scripts/load_balancing/fdm_omp.sh
@@ -6,14 +6,12 @@
 #SBATCH -J fdm_omp          # Job name
 #SBATCH -o fdm_omp.out      # Name of stdout output file
 #SBATCH -e fdm_omp.err      # Name of stderr output file
-#SBATCH -N 4                # Total # of nodes
+#SBATCH -N 8                # Total # of nodes
 #SBATCH --cpus-per-task 4   # Cores per task (for OpenMP threads)
-#SBATCH -n 4                # Total # of MPI tasks
+#SBATCH -n 8                # Total # of MPI tasks
 #SBATCH --ntasks-per-node=1 # 1 MPI task per node
 #SBATCH -t 1:00:00          # Run time (hh:mm:ss) - 1 hour
-#SBATCH --nodelist=n16-82,n16-83,n16-92,n16-93
 #SBATCH --exclusive
-
 
 export OMP_NUM_THREADS=4
 
